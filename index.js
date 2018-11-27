677858cfb12aeee489044656232917223435c988
Henry Darnell <hjdarnel@users.noreply.github.com>
2018-11-27 15:04:41 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const fs = require('fs');
+
+const request = require('request');
+const eachOfLimit = require('async/eachOfLimit');
+
+const token = process.env.TOKEN;
+const url = `https://slack.com/api/emoji.list?token=${token}`;
+const directory = './static/';
+
+const downloadEmoji = (item, key, callback) => {
+    if (item.startsWith('alias:')) {
+        callback();
+    } else {
+        const r = request(item);
+        r.on('response', (res) => {
+            res.pipe(fs.createWriteStream(directory + key + '.' + res.headers['content-type'].split('/')[1]));
+        });
+        callback();
+    }
+}
+
+
+request(url, (error, response, body) => {
+    const res = JSON.parse(body);
+    eachOfLimit(res.emoji, 5, downloadEmoji);
+});
